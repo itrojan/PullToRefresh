6a83876683da79892ddf64c9779d2a1236d67a18
itrojan <itrojan@126.com>
2016-10-12 15:53:07 +0800

    1.0.6

--- pulltorefresh/src/main/java/com/aihook/pulltorefresh/lib/XListView.java
+++ pulltorefresh/src/main/java/com/aihook/pulltorefresh/lib/XListView.java
@@ -429,7 +429,7 @@ public class XListView extends ListView implements OnScrollListener {
             mHandler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    if (mEnablePullLoad && null != mListener) {
+                    if (mEnablePullRefresh && null != mListener) {
                         mListener.onRefresh();
                     }
 
